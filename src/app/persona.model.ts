d217d3b035872d18007883b543540176abcecbf9
Marcela-Sevilla <marcelasevilla514@gmail.com>
2023-02-22 22:04:53 -0500

    Creando archivo modelo de TypeScript y aprendiendo primera forma para crear el constructor

---
+++ src/app/persona.model.ts
@@ -0,0 +1,10 @@
+export class Persona{
+    // Primera Forma de crear el metodo contructor
+    // nombre:string = '';
+    // apellidos:string = '';
+
+    // constructor(nombre:string, apellidos:string){
+    //     this.nombre
+    //     this.apellidos
+    // }
+}
