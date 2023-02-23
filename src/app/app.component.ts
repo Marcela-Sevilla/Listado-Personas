946762cc7eafe6e8c34dce09a3a8cad0579b73bd
Marcela-Sevilla <marcelasevilla514@gmail.com>
2023-02-22 23:36:27 -0500

    Agregando personas de forma dinamica al array de Personas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,18 @@ import { Persona } from './persona.model';
 export class AppComponent {
   titulo = 'Listado de Personas';
 
-  personas:Persona[] = [new Persona('Marcela', 'Sevilla Lopez'),  new Persona('Natalia', 'Jaller Lopez')];
-  
+  personas:Persona[] = [
+    new Persona('Marcela', 'Sevilla Lopez'),  
+    new Persona('Natalia', 'Jaller Lopez'), 
+    new Persona('Daniela', 'Cavadiaz Lopez'),
+  ];
+
+  nombreInput:string = '';
+  apellidosInput:string = '';
+
+  agregarPersona(){
+    let persona1 = new Persona(this.nombreInput, this.apellidosInput);
+    this.personas.push(persona1);
+  }
 
 }
