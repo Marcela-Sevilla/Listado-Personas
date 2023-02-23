93560a6f5f2a9830a64ac20ce52ac88a1139ab56
Marcela-Sevilla <marcelasevilla514@gmail.com>
2023-02-22 22:12:04 -0500

    Creando instancia de la clase Persona(objetos personas)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   titulo = 'Listado de Personas';
+
+  personas:Persona[] = [new Persona('Marcela', 'Sevilla Lopez'),  new Persona('Natalia', 'Jaller Lopez')];
+
 }
