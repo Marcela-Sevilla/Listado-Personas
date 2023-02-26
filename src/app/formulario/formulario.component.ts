deae4e94e320367282cd7a57cb985046052e6274
Marcela-Sevilla <marcelasevilla514@gmail.com>
2023-02-26 18:12:21 -0500

    Utilizando dependencias de inyección con el decorador @Injectable

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { loginService } from '../loginService.services';
 import { personasServices } from '../personasServices.services';
 import { Persona } from '../persona.model';
 
@@ -14,13 +13,10 @@ export class FormularioComponent {
   @ViewChild('nombreInput') nombreInput:ElementRef;
   @ViewChild('apellidosInput') apellidosInput:ElementRef;
 
-  constructor(
-    private loginService:loginService,
-    private personasServices:personasServices){}
+  constructor(private personasServices:personasServices){}
 
   agregarPersona(){
     let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidosInput.nativeElement.value);
-    this.loginService.enviarMensajeConsola(`Envío de persona: ${persona1.nombre} ${persona1.apellidos}`);
     this.personasServices.personaAgregada(persona1)
   }
 
--- src/app/personasServices.services.ts
+++ src/app/personasServices.services.ts
@@ -1,5 +1,8 @@
 import { Persona } from "./persona.model";
+import { loginService } from './loginService.services';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class personasServices{
     personas:Persona[] = [
         new Persona('Marcela', 'Sevilla Lopez'),  
@@ -7,7 +10,10 @@ export class personasServices{
         new Persona('Daniela', 'Cavadiaz Lopez'),
     ];
 
+    constructor(private loginService:loginService){}
+
     personaAgregada(persona:Persona){
         this.personas.push(persona);
+        this.loginService.enviarMensajeConsola(`Envío de persona: ${persona.nombre} ${persona.apellidos}`);
     }
 }
